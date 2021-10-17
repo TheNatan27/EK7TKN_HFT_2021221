78c6d1e72fcbeeec2cf337da6afd1b3fa3880543
Natan <gynatan98@gmail.com>
2021-10-17 12:18:14 +0200

    Database fill 01

---
+++ EK7TKN_HFT_2021221.Models/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EK7TKN_HFT_2021221.Models
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            UserInformationContext use = new UserInformationContext();
+
+            use.Users.Add(new UserInformation() { UserID = 01, Weight = 77 });
+            use.Users.Add(new UserInformation() { UserID = 02, Weight = 55 });
+            use.Users.Add(new UserInformation() { UserID = 03, Weight = 79 });
+            use.Users.Add(new UserInformation() { UserID = 04, Weight = 87 });
+
+            use.SaveChanges();
+
+            foreach (var item in use.Users)
+            {
+                Console.WriteLine(item.UserID + item.Weight);
+            }
+
+
+        }
+    }
+}
--- EK7TKN_HFT_2021221.Models/UserInformation.cs
+++ EK7TKN_HFT_2021221.Models/UserInformation.cs
@@ -12,11 +12,11 @@ namespace EK7TKN_HFT_2021221.Models
     {
         [Key]
         public int UserID { get; set; }
-        public string First_Name { get; set; }
-        public string Last_Name { get; set; }
-        public int Age { get; set; }
+        //public string First_Name { get; set; }
+        //public string Last_Name { get; set; }
+        //public int Age { get; set; }
         public double Weight { get; set; }
-        public double Height { get; set; }
+        //public double Height { get; set; }
 
 
     }
