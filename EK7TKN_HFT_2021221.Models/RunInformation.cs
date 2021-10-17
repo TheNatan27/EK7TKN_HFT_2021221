5d0a30c6749a4d8c63c9cd99a1661676fea0f2bd
Natan <gynatan98@gmail.com>
2021-10-17 11:49:41 +0200

    RunInformation class created

--- EK7TKN_HFT_2021221.Models/RunInformation.cs
+++ EK7TKN_HFT_2021221.Models/RunInformation.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EK7TKN_HFT_2021221.Models
 {
-    class RunInformation
+    public class RunInformation
     {
+        [Key]
+        public int UserID { get; set; }
+        public double Distance { get; set; }
+        public string Time { get; set; }
+        
+
+
+    }
+
+    public class RunInformationContext : DbContext
+    {
+        public virtual DbSet<RunInformation> Runs { get; set; }
+
+        public RunInformationContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+
+            }
+        }
     }
 }
