45fe98893d0038aa9c8f69722a7a337230b5c24c
Natan <gynatan98@gmail.com>
2021-10-17 10:49:56 +0200

    EntityFrameworks imported

--- EK7TKN_HFT_2021221.Data/Class1.cs
+++ EK7TKN_HFT_2021221.Data/Class1.cs
@@ -4,5 +4,7 @@ namespace EK7TKN_HFT_2021221.Data
 {
     public class Class1
     {
+
+        //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True
     }
 }
